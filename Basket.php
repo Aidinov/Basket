abec863b9c8cd159f6419e405e5f69c4038ba072
Aidinov <gabosss0@gmail.com>
2018-02-21 17:06:44 +0500

    Initial commit

---
+++ Basket.php
@@ -0,0 +1,43 @@
+<?php
+namespace Aidinov\Basket;
+
+class Basket
+{
+    public function has(): bool
+    {
+
+    }
+
+    public function contents(): array
+    {
+
+    }
+
+    public function item(int $id): Item
+    {
+
+    }
+
+    public function add(Item $item) 
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function weight() {
+
+    }
+    
+    public function total()
+    {
+
+    }
+    
+    public function totalItems() {
+
+    }
+}
